062859e499c8cce16030cc46ee6955733ad1a779
Eugene F. Barker <genebarker@gmail.com>
2019-04-03 12:23:35 +1400

    Fix MemoryAdapter to use oid for AppUser key

--- shrike/adapters/memory_adapter.py
+++ shrike/adapters/memory_adapter.py
@@ -56,21 +56,31 @@ class MemoryAdapter(StorageProvider):
         self.app_user_next_id += 1
         return next_id
 
-    def get_app_user(self, username):
-        if username not in self.app_user:
+    def get_app_user_by_username(self, username):
+        oid = self._get_app_user_id_for_username(username)
+        if oid is None:
             message = 'app_user (username={}) does not exist'.format(username)
             raise KeyError(message)
-        app_user = self.app_user[username]
+        app_user = self.app_user[oid]
         return copy.copy(app_user)
 
+    def _get_app_user_id_for_username(self, username):
+        for oid, app_user in self.app_user.items():
+            if app_user.username == username:
+                return oid
+        return None
+
     def add_app_user(self, app_user):
-        if app_user.username in self.app_user:
+        if self.exists_app_username(app_user.username):
             message = 'app_user (username={}) already exists'.format(app_user.username)
             raise ValueError(message)
-        self.app_user[app_user.username] = copy.copy(app_user)
+        if app_user.oid in self.app_user:
+            message = 'app_user (oid={}) already exists'.format(app_user.oid)
+            raise ValueError(message)
+        self.app_user[app_user.oid] = copy.copy(app_user)
 
     def update_app_user(self, app_user):
-        self.app_user[app_user.username] = app_user
+        self.app_user[app_user.oid] = app_user
     
-    def exists_app_user(self, username):
-        return (username in self.app_user)
+    def exists_app_username(self, username):
+        return self._get_app_user_id_for_username(username) is not None
--- tests/adapters/test_memory_adapter.py
+++ tests/adapters/test_memory_adapter.py
@@ -92,13 +92,13 @@ class TestMemoryAdapter:
 
     #region - test app_user methods
 
-    def test_get_unknown_raises(self):
+    def test_get_app_user_unknown_raises(self):
         with pytest.raises(KeyError, match='app_user .username=xyz. does not exist'):
-            self.storage_provider.get_app_user('xyz')
+            self.storage_provider.get_app_user_by_username('xyz')
 
-    def test_get_gets_record(self):
-        original_user = self.add_test_app_user('getGETS')
-        stored_user = self.storage_provider.get_app_user('getGETS')
+    def test_get_app_user_by_username_gets_record(self):
+        original_user = self.add_test_app_user('getAppUserGETS')
+        stored_user = self.storage_provider.get_app_user_by_username('getAppUserGETS')
         assert stored_user == original_user
 
     def add_test_app_user(self, username):
@@ -110,51 +110,57 @@ class TestMemoryAdapter:
         self.storage_provider.add_app_user(app_user)
         return app_user
 
-    def test_get_returns_a_copy(self):
-        self.add_test_app_user('getRETURNS')
-        copied_user = self.storage_provider.get_app_user('getRETURNS')
+    def test_get_app_user_returns_a_copy(self):
+        self.add_test_app_user('getAppUserRETURNS')
+        copied_user = self.storage_provider.get_app_user_by_username('getAppUserRETURNS')
         copied_user.name = 'Different'
-        stored_user = self.storage_provider.get_app_user('getRETURNS')
+        stored_user = self.storage_provider.get_app_user_by_username('getAppUserRETURNS')
         assert stored_user != copied_user
 
-    def test_add_adds_record(self):
-        original_user = self.add_test_app_user('addADDS')
-        stored_user = self.storage_provider.get_app_user('addADDS')
+    def test_add_app_user_adds_record(self):
+        original_user = self.add_test_app_user('addAppUserADDS')
+        stored_user = self.storage_provider.get_app_user_by_username('addAppUserADDS')
         assert stored_user == original_user
 
-    def test_add_adds_a_copy(self):
-        some_user = self.add_test_app_user('addsCOPY')
+    def test_add_app_user_adds_a_copy(self):
+        some_user = self.add_test_app_user('addAppUserAddsCOPY')
         some_user.name = 'Different'
-        stored_user = self.storage_provider.get_app_user('addsCOPY')
+        stored_user = self.storage_provider.get_app_user_by_username('addAppUserAddsCOPY')
         assert stored_user != some_user
 
-    def test_add_duplicate_raises(self):
-        some_user = self.add_test_app_user('addDUPE')
-        with pytest.raises(ValueError, match='app_user .username=addDUPE. already exists'):
+    def test_add_app_user_with_duplicate_username_raises(self):
+        some_user = self.add_test_app_user('addAppUserDupeUsername')
+        with pytest.raises(ValueError, match='app_user .username=addAppUserDupeUsername. already exists'):
             self.storage_provider.add_app_user(some_user)
 
-    def test_update_updates_record(self):
-        some_user = self.add_test_app_user('updateUpdates')
+    def test_add_app_user_with_duplicate_oid_raises(self):
+        some_user = self.add_test_app_user('addAppUserDupeOID')
+        with pytest.raises(ValueError, match='app_user .oid={0}. already exists'.format(some_user.oid)):
+            some_user.username = 'Different'
+            self.storage_provider.add_app_user(some_user)
+
+    def test_update_app_user_updates_record(self):
+        some_user = self.add_test_app_user('updateAppUserUpdates')
         some_user.name = "DIFFERENT"
         self.storage_provider.update_app_user(some_user)
-        stored_user = self.storage_provider.get_app_user('updateUpdates')
+        stored_user = self.storage_provider.get_app_user_by_username('updateAppUserUpdates')
         assert stored_user == some_user
 
-    def test_exists_true_for_known(self):
-        self.add_test_app_user('existsTRUE')
-        assert self.storage_provider.exists_app_user('existsTRUE')
+    def test_exists_app_user_true_for_known(self):
+        self.add_test_app_user('existsAppUserTRUE')
+        assert self.storage_provider.exists_app_username('existsAppUserTRUE')
 
-    def test_exists_false_for_unknown(self):
-        assert self.storage_provider.exists_app_user('existsUNKNOWN') is False
+    def test_exists_app_user_false_for_unknown(self):
+        assert self.storage_provider.exists_app_username('existsAppUserUNKNOWN') is False
 
-    def test_add_exists_after_commit(self):
-        self.add_test_app_user('commitWORKS')
+    def test_add_app_user_record_exists_after_commit(self):
+        self.add_test_app_user('commitAppUserWORKS')
         self.storage_provider.commit()
-        assert self.storage_provider.exists_app_user('commitWORKS')
+        assert self.storage_provider.exists_app_username('commitAppUserWORKS')
 
-    def test_add_gone_after_rollback(self):
-        self.add_test_app_user('rollbackWORKS')
+    def test_add_app_user_record_gone_after_rollback(self):
+        self.add_test_app_user('rollbackAppUserWORKS')
         self.storage_provider.rollback()
-        assert not self.storage_provider.exists_app_user('rollbackWORKS')
+        assert not self.storage_provider.exists_app_username('rollbackAppUserWORKS')
 
     #endregion
