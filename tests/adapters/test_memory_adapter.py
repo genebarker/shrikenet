feb703044a57b8068a94bbb427860c2cb0179906
Eugene F. Barker <genebarker@gmail.com>
2019-04-03 10:17:26 +1400

    Refactor tests to get oid via storage provider

--- tests/adapters/test_memory_adapter.py
+++ tests/adapters/test_memory_adapter.py
@@ -97,71 +97,63 @@ class TestMemoryAdapter:
             self.storage_provider.get_app_user('xyz')
 
     def test_get_gets_record(self):
-        original_user = self.create_test_app_user('getGETS')
-        self.storage_provider.add_app_user(original_user)
+        original_user = self.add_test_app_user('getGETS')
         stored_user = self.storage_provider.get_app_user('getGETS')
         assert stored_user == original_user
 
-    @staticmethod
-    def create_test_app_user(username):
+    def add_test_app_user(self, username):
         name = 'The ' + username
         password_hash = 'xxx' + username + 'YYY'
         app_user = AppUser(username, name, password_hash)
+        oid = self.storage_provider.get_next_app_user_id()
+        app_user.oid = oid
+        self.storage_provider.add_app_user(app_user)
         return app_user
 
     def test_get_returns_a_copy(self):
-        original_user = self.create_test_app_user('getRETURNS')
-        self.storage_provider.add_app_user(original_user)
-
+        self.add_test_app_user('getRETURNS')
         copied_user = self.storage_provider.get_app_user('getRETURNS')
         copied_user.name = 'Different'
         stored_user = self.storage_provider.get_app_user('getRETURNS')
         assert stored_user != copied_user
 
     def test_add_adds_record(self):
-        original_user = self.create_test_app_user('addADDS')
-        self.storage_provider.add_app_user(original_user)
+        original_user = self.add_test_app_user('addADDS')
         stored_user = self.storage_provider.get_app_user('addADDS')
         assert stored_user == original_user
 
     def test_add_adds_a_copy(self):
-        some_user = self.create_test_app_user('addsCOPY')
-        self.storage_provider.add_app_user(some_user)
+        some_user = self.add_test_app_user('addsCOPY')
         some_user.name = 'Different'
         stored_user = self.storage_provider.get_app_user('addsCOPY')
         assert stored_user != some_user
 
     def test_add_duplicate_raises(self):
-        some_user = self.create_test_app_user('addDUPE')
-        self.storage_provider.add_app_user(some_user)
+        some_user = self.add_test_app_user('addDUPE')
         with pytest.raises(ValueError, match='app_user .username=addDUPE. already exists'):
             self.storage_provider.add_app_user(some_user)
 
     def test_update_updates_record(self):
-        some_user = self.create_test_app_user('updateUpdates')
-        self.storage_provider.add_app_user(some_user)
+        some_user = self.add_test_app_user('updateUpdates')
         some_user.name = "DIFFERENT"
         self.storage_provider.update_app_user(some_user)
         stored_user = self.storage_provider.get_app_user('updateUpdates')
         assert stored_user == some_user
 
     def test_exists_true_for_known(self):
-        some_user = self.create_test_app_user('existsTRUE')
-        self.storage_provider.add_app_user(some_user)
+        self.add_test_app_user('existsTRUE')
         assert self.storage_provider.exists_app_user('existsTRUE')
 
     def test_exists_false_for_unknown(self):
         assert self.storage_provider.exists_app_user('existsUNKNOWN') is False
 
     def test_add_exists_after_commit(self):
-        original_user = self.create_test_app_user('commitWORKS')
-        self.storage_provider.add_app_user(original_user)
+        self.add_test_app_user('commitWORKS')
         self.storage_provider.commit()
         assert self.storage_provider.exists_app_user('commitWORKS')
 
     def test_add_gone_after_rollback(self):
-        original_user = self.create_test_app_user('rollbackWORKS')
-        self.storage_provider.add_app_user(original_user)
+        self.add_test_app_user('rollbackWORKS')
         self.storage_provider.rollback()
         assert not self.storage_provider.exists_app_user('rollbackWORKS')
 
