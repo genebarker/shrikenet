27513c5c60aa04fe766ffaac0753926e5285b739
Eugene F. Barker <genebarker@gmail.com>
2019-04-04 13:29:32 +1400

    Add equals operator to Post

--- shrike/entities/post.py
+++ shrike/entities/post.py
@@ -2,7 +2,15 @@ class Post:
 
     def __init__(self, title, body, oid=None, author_oid=None, created_time=None):
         self.oid = oid
-        self.author_oid = author_oid
-        self.created_time = created_time
         self.title = title
         self.body = body
+        self.author_oid = author_oid
+        self.created_time = created_time
+
+    def __eq__(self, other):
+        return (isinstance(other, Post) and
+                self.oid == other.oid and
+                self.title == other.title and
+                self.body == other.body and
+                self.author_oid == other.author_oid and
+                self.created_time == other.created_time)
--- tests/entities/test_post.py
+++ tests/entities/test_post.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 import pytest
 
@@ -11,6 +11,15 @@ GOOD_CREATED_TIME = datetime(2018, 12, 31, 23, 58, tzinfo=timezone.utc)
 GOOD_TITLE = 'The World is Doomed'
 GOOD_BODY = 'No hot sauce was found in the house.'
 
+def create_good_post():
+    return Post(
+            oid=GOOD_OID,
+            title=GOOD_TITLE,
+            body=GOOD_BODY,
+            author_oid=GOOD_AUTHOR_OID,
+            created_time=GOOD_CREATED_TIME,
+        )
+    
 class TestGeneralProperties:
 
     def test_minimal_init(self):
@@ -34,3 +43,39 @@ class TestGeneralProperties:
         assert post.body == GOOD_BODY
         assert post.author_oid == GOOD_AUTHOR_OID
         assert post.created_time == GOOD_CREATED_TIME
+
+class TestEquals:
+
+    def test_equals(self):
+        post_one = create_good_post()
+        post_two = create_good_post()
+        assert post_one == post_two
+
+    def test_unequal_when_class_different(self):
+        class FakePost:
+            def __init__(self, title, body):
+                self.title = title
+                self.body = body
+        
+        post_one = Post(GOOD_TITLE, GOOD_BODY)
+        post_two = FakePost(GOOD_TITLE, GOOD_BODY)
+        assert post_one != post_two
+
+    @pytest.mark.parametrize(('oid', 'title', 'body', 'author_oid', 'created_time'), (
+        (999, GOOD_TITLE, GOOD_BODY, GOOD_AUTHOR_OID, GOOD_CREATED_TIME),
+        (GOOD_OID, 'Different', GOOD_BODY, GOOD_AUTHOR_OID, GOOD_CREATED_TIME),
+        (GOOD_OID, GOOD_TITLE, 'Different', GOOD_AUTHOR_OID, GOOD_CREATED_TIME),
+        (GOOD_OID, GOOD_TITLE, GOOD_BODY, 999, GOOD_CREATED_TIME),
+        (GOOD_OID, GOOD_TITLE, GOOD_BODY, GOOD_AUTHOR_OID, GOOD_CREATED_TIME + timedelta(seconds=1)),
+    ))
+    def test_unequal_when_attribute_different(
+            self, oid, title, body, author_oid, created_time):
+        post_one = create_good_post()
+        post_two = Post(
+            oid=oid,
+            title=title,
+            body=body,
+            author_oid=author_oid,
+            created_time=created_time,
+        )
+        assert post_one != post_two
