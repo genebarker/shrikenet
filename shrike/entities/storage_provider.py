440931085144d41d26e667f5b0933808dc8b8d2f
Eugene F. Barker <genebarker@gmail.com>
2019-06-21 13:45:28 -0600

    Rework abstraction technique used for StorageProvider
    Makes code cleaner and works better with test coverage tool.

--- shrike/entities/storage_provider.py
+++ shrike/entities/storage_provider.py
@@ -1,91 +1,67 @@
-from abc import ABC, abstractmethod
+class StorageProvider:
 
-class StorageProvider(ABC):
-
-    @abstractmethod
     def __init__(self, db_config):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def open(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def close(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def commit(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def rollback(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def build_database_schema(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def reset_database_objects(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_version(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_next_app_user_oid(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_next_post_oid(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_app_user_by_username(self, username):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_app_user_by_oid(self, oid):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def add_app_user(self, app_user):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
     
-    @abstractmethod
     def update_app_user(self, app_user):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_app_user_count(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def exists_app_username(self, username):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_post_by_oid(self, oid):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def add_post(self, post):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def update_post(self, post):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def delete_post_by_oid(self, oid):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_post_count(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
 
-    @abstractmethod
     def get_posts(self):
-        raise NotImplementedError #pragma: no cover
+        raise NotImplementedError
---
+++ tests/entities/test_storage_provider.py
@@ -0,0 +1,46 @@
+import pytest
+
+from shrike.entities.storage_provider import StorageProvider
+
+class TestStorageProvider:
+
+    def test_cant_be_instantiated(self):
+        with pytest.raises(NotImplementedError):
+            StorageProvider(None)
+
+    @pytest.fixture
+    def storage_provider(self):
+        class FakeProvider(StorageProvider):
+            def __init__(self, dbonfig=None):
+                pass
+        return FakeProvider()
+
+    @pytest.mark.parametrize(('method_name', 'args'), (
+        ('open', 0),
+        ('close', 0),
+        ('commit', 0),
+        ('rollback', 0),
+        ('build_database_schema', 0),
+        ('reset_database_objects', 0),
+        ('get_version', 0),
+        ('get_next_app_user_oid', 0),
+        ('get_next_post_oid', 0),
+        ('get_app_user_by_username', 1),
+        ('get_app_user_by_oid', 1),
+        ('add_app_user', 1),
+        ('update_app_user', 1),
+        ('get_app_user_count', 0),
+        ('exists_app_username', 1),
+        ('get_post_by_oid', 1),
+        ('add_post', 1),
+        ('update_post', 1),
+        ('delete_post_by_oid', 1),
+        ('get_post_count', 0),
+        ('get_posts', 0),
+    ))
+    def test_method_is_uncallable(self, storage_provider, method_name, args):
+        method = getattr(storage_provider, method_name)
+        with pytest.raises(NotImplementedError):
+            if args == 0: method()
+            if args == 1: method(None)
+            pass
