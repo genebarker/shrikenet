2c92b8c0bc772f62bf2515e57961827be322e253
Eugene F. Barker <genebarker@gmail.com>
2019-10-03 14:45:35 +1400

    Add CryptoProvider to Services

--- shrike/entities/services.py
+++ shrike/entities/services.py
@@ -1,5 +1,6 @@
 class Services:
 
-    def __init__(self, storage_provider, text_transformer):
+    def __init__(self, storage_provider=None, text_transformer=None, crypto_provider=None):
         self.storage_provider = storage_provider
         self.text_transformer = text_transformer
+        self.crypto_provider = crypto_provider
