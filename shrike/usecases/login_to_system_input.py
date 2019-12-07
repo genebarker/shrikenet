a74fc2531672f1991031f375dd2452a68a1db23a
Eugene F. Barker <genebarker@gmail.com>
2019-12-08 13:15:33 +1400

    Remove unused use case helper class

--- shrike/usecases/login_to_system_input.py
+++
@@ -1,6 +0,0 @@
-class LoginToSystemInput:
-
-    def __init__(self, username, password, services):
-        self.username = username
-        self.password = password
-        self.services = services
