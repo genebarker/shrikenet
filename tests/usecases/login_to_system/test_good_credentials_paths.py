429e202946f36a09dd8d5e0a863965328bb6c09a
Eugene F. Barker <genebarker@gmail.com>
2020-03-26 22:57:44 +1400

    Ensure user record change on login is commited

--- tests/usecases/login_to_system/test_good_credentials_paths.py
+++ tests/usecases/login_to_system/test_good_credentials_paths.py
@@ -29,11 +29,20 @@ class TestGoodCredentialPaths(SetupClass):
         )
 
     def test_password_fail_count_reset_on_successful_login(self):
+        self.perform_login_to_reset_password_fail_count()
+        user = self.db.get_app_user_by_username(GOOD_USER_USERNAME)
+        assert user.ongoing_password_failure_count == 0
+
+    def perform_login_to_reset_password_fail_count(self):
         user = self.create_good_user()
         user.ongoing_password_failure_count = 2
         self.db.update_app_user(user)
         login_to_system = LoginToSystem(self.services)
         login_to_system.run(GOOD_USER_USERNAME, GOOD_USER_PASSWORD,
                             GOOD_IP_ADDRESS)
-        user_after = self.db.get_app_user_by_username(GOOD_USER_USERNAME)
-        assert user_after.ongoing_password_failure_count == 0
+
+    def test_user_record_change_is_committed(self):
+        self.perform_login_to_reset_password_fail_count()
+        self.db.rollback()
+        user = self.db.get_app_user_by_username(GOOD_USER_USERNAME)
+        assert user.ongoing_password_failure_count == 0
