4142d311f11f4d21ba885e13f4f769a5be2ed13a
Eugene F. Barker <genebarker@gmail.com>
2020-06-10 13:13:59 +1300

    Improve variable name and tidy layout

--- tests/entities/test_event.py
+++ tests/entities/test_event.py
@@ -9,7 +9,7 @@ CHRISTMAS_2018 = datetime(2018, 12, 25, 0, 0, tzinfo=timezone.utc)
 
 
 def test_base_field_initialization():
-    e = Event(
+    event = Event(
         oid=2112,
         time=CHRISTMAS_2018,
         app_user_oid=123,
@@ -17,16 +17,17 @@ def test_base_field_initialization():
         text='fmulder found a duck.',
         usecase_tag='look_for_duck',
     )
-    assert e.oid == 2112
-    assert e.time == CHRISTMAS_2018
-    assert e.app_user_oid == 123
-    assert e.tag == 'duck_found'
-    assert e.text == 'fmulder found a duck.'
-    assert e.usecase_tag == 'look_for_duck'
-    assert e.app_user_name is None
+    assert event.oid == 2112
+    assert event.time == CHRISTMAS_2018
+    assert event.app_user_oid == 123
+    assert event.tag == 'duck_found'
+    assert event.text == 'fmulder found a duck.'
+    assert event.usecase_tag == 'look_for_duck'
+    assert event.app_user_name is None
+
 
 def test_optional_field_initialization():
-    e = Event(
+    event = Event(
         oid=None,
         time=None,
         app_user_oid=None,
@@ -35,4 +36,4 @@ def test_optional_field_initialization():
         usecase_tag=None,
         app_user_name='Fox Mulder'
     )
-    assert e.app_user_name == 'Fox Mulder'
+    assert event.app_user_name == 'Fox Mulder'
