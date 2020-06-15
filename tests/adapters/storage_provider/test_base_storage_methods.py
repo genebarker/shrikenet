9616fe580ed244af423c0c2915b9338d802b79e4
Eugene F. Barker <genebarker@gmail.com>
2020-06-15 15:22:19 +1300

    Make test that get object returns a copy generic

--- tests/adapters/storage_provider/test_base_storage_methods.py
+++ tests/adapters/storage_provider/test_base_storage_methods.py
@@ -14,3 +14,19 @@ def test_get_object_by_oid_gets_record(db, create_method, get_method_name):
     get_obj_by_oid = getattr(db, get_method_name)
     stored_obj = get_obj_by_oid(obj.oid)
     assert stored_obj == obj
+
+
+@pytest.mark.parametrize(
+    'create_method, an_attr_name, get_method_name',
+    [
+        (create_user, 'name', 'get_app_user_by_oid'),
+    ],
+)
+def test_get_object_by_oid_returns_a_copy(db, create_method,
+                                          an_attr_name, get_method_name):
+    obj = create_method(db)
+    get_obj_by_oid = getattr(db, get_method_name)
+    copied_obj = get_obj_by_oid(obj.oid)
+    setattr(copied_obj, an_attr_name, 'Different')
+    stored_obj = get_obj_by_oid(obj.oid)
+    assert stored_obj != copied_obj
