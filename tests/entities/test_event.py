827b86e55a5e127c3a856f9227dd2fe780fe2d00
Eugene F. Barker <genebarker@gmail.com>
2020-06-18 12:32:17 +1300

    Extract create event method for reuse

--- tests/entities/test_event.py
+++ tests/entities/test_event.py
@@ -9,14 +9,7 @@ CHRISTMAS_2018 = datetime(2018, 12, 25, 0, 0, tzinfo=timezone.utc)
 
 
 def test_base_field_initialization():
-    event = Event(
-        oid=2112,
-        time=CHRISTMAS_2018,
-        app_user_oid=123,
-        tag='duck_found',
-        text='fmulder found a duck.',
-        usecase_tag='look_for_duck',
-    )
+    event = create_good_event()
     assert event.oid == 2112
     assert event.time == CHRISTMAS_2018
     assert event.app_user_oid == 123
@@ -24,6 +17,17 @@ def test_base_field_initialization():
     assert event.text == 'fmulder found a duck.'
     assert event.usecase_tag == 'look_for_duck'
     assert event.app_user_name is None
+
+
+def create_good_event():
+    return Event(
+        oid=2112,
+        time=CHRISTMAS_2018,
+        app_user_oid=123,
+        tag='duck_found',
+        text='fmulder found a duck.',
+        usecase_tag='look_for_duck',
+    )
 
 
 def test_optional_field_initialization():
