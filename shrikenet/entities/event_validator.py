8fc4063a0f2e4c8ad47c096b4d004549570c3543
Eugene F. Barker <genebarker@gmail.com>
2020-06-18 13:30:39 +1300

    Start validate fields implementation

--- shrikenet/entities/event_validator.py
+++ shrikenet/entities/event_validator.py
@@ -1,5 +1,10 @@
+from shrikenet.entities.event import Event
+from shrikenet.entities.field_validator import FieldValidator
 from shrikenet.entities.record_validator import RecordValidator
 
 
 class EventValidator(RecordValidator):
-    pass
+
+    @staticmethod
+    def validate_fields(the_object: Event):
+        FieldValidator.validate_oid(the_object.oid)
--- tests/entities/test_event_validator.py
+++ tests/entities/test_event_validator.py
@@ -3,6 +3,24 @@ import pytest
 from shrikenet.entities.event_validator import EventValidator
 from shrikenet.entities.record_validator import RecordValidator
 
+from tests.entities.test_event import create_good_event
+
 
 def test_is_a_record_validator():
     assert issubclass(EventValidator, RecordValidator)
+
+
+def test_successful_validation_returns_none():
+    event = create_good_event()
+    assert EventValidator.validate_fields(event) is None
+
+
+def test_oid_required():
+    event = create_good_event()
+    event.oid = None
+    verify_validation_raises(event)
+
+
+def verify_validation_raises(event):
+    with pytest.raises(ValueError):
+        EventValidator.validate_fields(event)
