7cc0c2c320f09c9f3d5910669e9203e6933d56c6
Eugene F. Barker <genebarker@gmail.com>
2020-06-18 14:20:54 +1300

    Add fixture to simplify event validator tests

--- tests/entities/test_event_validator.py
+++ tests/entities/test_event_validator.py
@@ -6,17 +6,21 @@ from shrikenet.entities.record_validator import RecordValidator
 from tests.entities.test_event import create_good_event
 
 
+# pylint: disable=redefined-outer-name
+@pytest.fixture
+def event():
+    return create_good_event()
+
+
 def test_is_a_record_validator():
     assert issubclass(EventValidator, RecordValidator)
 
 
-def test_successful_validation_returns_none():
-    event = create_good_event()
+def test_successful_validation_returns_none(event):
     assert EventValidator.validate_fields(event) is None
 
 
-def test_oid_required():
-    event = create_good_event()
+def test_oid_required(event):
     event.oid = None
     verify_validation_raises(event)
 
@@ -26,19 +30,16 @@ def verify_validation_raises(event):
         EventValidator.validate_fields(event)
 
 
-def test_oid_validated():
-    event = create_good_event()
+def test_oid_validated(event):
     event.oid = 'bad'
     verify_validation_raises(event)
 
 
-def test_time_required():
-    event = create_good_event()
+def test_time_required(event):
     event.time = None
     verify_validation_raises(event)
 
 
-def test_time_validated():
-    event = create_good_event()
+def test_time_validated(event):
     event.time = 123
     verify_validation_raises(event)
