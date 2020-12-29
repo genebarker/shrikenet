bbd2510d9b17298e55c221739ea63d06c2fd0150
Eugene F. Barker <genebarker@gmail.com>
2020-12-29 15:09:33 +1400

    Remove unnecessary class and improve code spacing

--- tests/entities/test_text_transformer.py
+++ tests/entities/test_text_transformer.py
@@ -2,19 +2,20 @@ import pytest
 
 from shrikenet.entities.text_transformer import TextTransformer
 
-class TestTextTransformer:
 
-    def test_interface_cant_be_instantiated(self):
-        with pytest.raises(NotImplementedError):
-            TextTransformer()
+def test_interface_cant_be_instantiated():
+    with pytest.raises(NotImplementedError):
+        TextTransformer()
 
-    @pytest.fixture
-    def text_transformer(self):
-        class FakeTransformer(TextTransformer):
-            def __init__(self):
-                pass
-        return FakeTransformer()
 
-    def test_transform_method_cant_be_called(self, text_transformer):
-        with pytest.raises(NotImplementedError):
-            text_transformer.transform_to_html(None)
+@pytest.fixture
+def text_transformer():
+    class FakeTransformer(TextTransformer):
+        def __init__(self):
+            pass
+    return FakeTransformer()
+
+
+def test_transform_method_cant_be_called(text_transformer):
+    with pytest.raises(NotImplementedError):
+        text_transformer.transform_to_html(None)
