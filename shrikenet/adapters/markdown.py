f787195d9641fa462775eeedc3930e712154fe5b
Eugene F. Barker <genebarker@gmail.com>
2020-12-30 15:51:25 +1400

    Switch to python markdown with pymdown extensions

--- shrikenet/adapters/markdown.py
+++ shrikenet/adapters/markdown.py
@@ -1,4 +1,4 @@
-import mistune
+import markdown
 
 from shrikenet.entities.text_transformer import TextTransformer
 
@@ -6,12 +6,27 @@ from shrikenet.entities.text_transformer import TextTransformer
 class Markdown(TextTransformer):
 
     def __init__(self):
-        self.markdown = mistune.create_markdown(
-            plugins=['strikethrough', 'table', 'footnotes']
+        self.markdown = markdown.Markdown(
+            extensions=[
+                'abbr',
+                'fenced_code',
+                'footnotes',
+                'sane_lists',
+                'smarty',
+                'tables',
+                'pymdownx.tilde',
+                'pymdownx.caret',
+            ],
+            extension_configs={
+                'smarty': {
+                    'smart_quotes': False,
+                    'smart_angled_quotes': False,
+                }
+            }
         )
 
     def transform_to_html(self, plain_text):
         if plain_text is None:
             return ''
 
-        return self.markdown(plain_text)
+        return self.markdown.convert(plain_text)
--- tests/adapters/test_markdown.py
+++ tests/adapters/test_markdown.py
@@ -20,12 +20,27 @@ def test_empty_string_when_none(text_transformer):
 def test_performs_a_markdown_transform(text_transformer):
     assert (
         text_transformer.transform_to_html('*foo bar*')
-        == '<p><em>foo bar</em></p>\n'
+        == '<p><em>foo bar</em></p>'
     )
 
 
-def test_raw_html_escaped(text_transformer):
+def test_transforms_strikeout(text_transformer):
     assert (
-        text_transformer.transform_to_html('<b>foo bar</b>')
-        == '<p>&lt;b&gt;foo bar&lt;/b&gt;</p>\n'
+        text_transformer.transform_to_html('~~Strike this out.~~')
+        == '<p><del>Strike this out.</del></p>'
     )
+
+
+def test_transforms_subscript(text_transformer):
+    assert (
+        text_transformer.transform_to_html('H~2~O')
+        == '<p>H<sub>2</sub>O</p>'
+    )
+
+
+def test_transforms_superscript(text_transformer):
+    assert (
+        text_transformer.transform_to_html('10^2^ = 100')
+        == '<p>10<sup>2</sup> = 100</p>'
+    )
+
