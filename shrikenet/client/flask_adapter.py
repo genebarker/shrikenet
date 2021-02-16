0b7d43bfde06092fea26b9fb8122d894068fb852
Eugene F. Barker <genebarker@gmail.com>
2021-02-16 15:39:03 +1400

    Update flask adapter to return HTTPResponse obj's

--- shrikenet/client/flask_adapter.py
+++ shrikenet/client/flask_adapter.py
@@ -1,7 +1,16 @@
+from shrikenet.client.http_response import HTTPResponse
+
+
 class FlaskAdapter:
 
     def __init__(self, test_client):
         self.client = test_client
 
     def get(self, url):
-        return self.client.get(url)
+        flask_response = self.client.get(url)
+        json = flask_response.get_json() if flask_response.is_json else None
+        return HTTPResponse(
+            json=json,
+            status_code=flask_response.status_code,
+            status=flask_response.status,
+        )
--- tests/client/test_flask_adapter.py
+++ tests/client/test_flask_adapter.py
@@ -1,7 +1,21 @@
+from shrikenet.client.http_response import HTTPResponse
 from shrikenet.client.flask_adapter import FlaskAdapter
 
 
-def test_get_hello(client):
+def test_get_hello_returns_response_object(client):
     http = FlaskAdapter(client)
-    response = http.get('/hello')
-    assert b'Hello, World!' in response.data
+    response = http.get('/api/hello')
+    assert isinstance(response, HTTPResponse)
+    assert response.json == {
+        'error_code': 0,
+        'message': 'Hello, World!',
+    }
+    assert response.status_code == 200
+    assert response.status.lower() == '200 ok'
+
+
+def test_bad_get_returns_expected_status(client):
+    http = FlaskAdapter(client)
+    response = http.get('/NON/EXISTING/LINK')
+    assert response.status_code == 404
+    assert response.status.lower() == '404 not found'
