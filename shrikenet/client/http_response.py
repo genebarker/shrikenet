0209d79043fe66b42162e581271e17c5e7d62615
Eugene F. Barker <genebarker@gmail.com>
2021-02-16 15:36:50 +1400

    Fix default HTTP response status content

--- shrikenet/client/http_response.py
+++ shrikenet/client/http_response.py
@@ -1,6 +1,6 @@
 class HTTPResponse:
 
-    def __init__(self, json, status_code=200, status='OK'):
+    def __init__(self, json, status_code=200, status='200 OK'):
         self.json = json
         self.status_code = status_code
         self.status = status
