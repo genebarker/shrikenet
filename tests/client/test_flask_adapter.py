d2cd50bf3bdc03ff61e92489d4211590e9dd2ec6
Eugene F. Barker <genebarker@gmail.com>
2021-02-16 17:04:20 +1400

    Group adapter's tests into a class for reuse

--- tests/client/test_flask_adapter.py
+++ tests/client/test_flask_adapter.py
@@ -1,21 +1,30 @@
+import pytest
+
 from shrikenet.client.http_response import HTTPResponse
 from shrikenet.client.flask_adapter import FlaskAdapter
 
 
-def test_get_hello_returns_response_object(client):
-    http = FlaskAdapter(client)
-    response = http.get('/api/hello')
-    assert isinstance(response, HTTPResponse)
-    assert response.json == {
-        'error_code': 0,
-        'message': 'Hello, World!',
-    }
-    assert response.status_code == 200
-    assert response.status.lower() == '200 ok'
+class TestFlaskAdapter:
 
+    @staticmethod
+    def get_response_provider(client):
+        return FlaskAdapter(client)
 
-def test_bad_get_returns_expected_status(client):
-    http = FlaskAdapter(client)
-    response = http.get('/NON/EXISTING/LINK')
-    assert response.status_code == 404
-    assert response.status.lower() == '404 not found'
+    @pytest.fixture
+    def http(self, client):
+        return self.get_response_provider(client)
+
+    def test_get_hello_returns_response_object(self, http):
+        response = http.get('/api/hello')
+        assert isinstance(response, HTTPResponse)
+        assert response.json == {
+            'error_code': 0,
+            'message': 'Hello, World!',
+        }
+        assert response.status_code == 200
+        assert response.status.lower() == '200 ok'
+
+    def test_bad_get_returns_expected_status(self, http):
+        response = http.get('/NON/EXISTING/LINK')
+        assert response.status_code == 404
+        assert response.status.lower() == '404 not found'
