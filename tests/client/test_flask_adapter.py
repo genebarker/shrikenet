eae8a8bce637367db9eb4b4e71310f6f6697aa2f
Eugene F. Barker <genebarker@gmail.com>
2021-02-18 14:29:04 +1400

    Remove unneeded class grouping of tests

--- tests/client/test_flask_adapter.py
+++ tests/client/test_flask_adapter.py
@@ -5,30 +5,27 @@ from shrikenet.client.http_response import HTTPResponse
 from shrikenet.client.flask_adapter import FlaskAdapter
 
 
-class TestFlaskAdapter:
+@pytest.fixture
+def http(client):
+    yield FlaskAdapter(client)
 
-    @staticmethod
-    def get_response_provider(client):
-        return FlaskAdapter(client)
 
-    @pytest.fixture
-    def http(self, client):
-        return self.get_response_provider(client)
+def test_is_a_http_request_provider(http):
+    assert isinstance(http, HTTPRequestProvider)
 
-    def test_is_a_http_request_provider(self, http):
-        assert isinstance(http, HTTPRequestProvider)
 
-    def test_get_hello_returns_response_object(self, http):
-        response = http.get('/api/hello')
-        assert isinstance(response, HTTPResponse)
-        assert response.json == {
-            'error_code': 0,
-            'message': 'Hello, World!',
-        }
-        assert response.status_code == 200
-        assert response.status.lower().endswith('ok')
+def test_get_hello_returns_response_object(http):
+    response = http.get('/api/hello')
+    assert isinstance(response, HTTPResponse)
+    assert response.json == {
+        'error_code': 0,
+        'message': 'Hello, World!',
+    }
+    assert response.status_code == 200
+    assert response.status.lower().endswith('ok')
 
-    def test_bad_get_returns_expected_status(self, http):
-        response = http.get('/NON/EXISTING/LINK')
-        assert response.status_code == 404
-        assert response.status.lower().endswith('not found')
+
+def test_bad_get_returns_expected_status(http):
+    response = http.get('/NON/EXISTING/LINK')
+    assert response.status_code == 404
+    assert response.status.lower().endswith('not found')
