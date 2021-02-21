950e10f223f939d983a721c57346df729c902388
Eugene F. Barker <genebarker@gmail.com>
2021-02-22 13:33:16 +1400

    Improve test function name

--- tests/api/test_hello_json.py
+++ tests/api/test_hello_json.py
@@ -1,4 +1,4 @@
-def test_hello_returns_hello(client):
+def test_naked_get_says_hello(client):
     response = client.get('/api/hello')
     json = response.get_json()
     assert json['error_code'] == 0
