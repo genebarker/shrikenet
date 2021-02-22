3a07ee1ea64924603cceb39f3ebcfaf6cf490d99
Eugene F. Barker <genebarker@gmail.com>
2021-02-22 14:47:10 +1400

    Add a GET test method to the api

--- shrikenet/api/hello_json.py
+++ shrikenet/api/hello_json.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, g
+
+from shrikenet.api.token_authority import token_required
 
 
 bp = Blueprint('hello_json', __name__, url_prefix='/api')
@@ -9,4 +11,16 @@ def hello_json():
     return {
         'error_code': 0,
         'message': 'Hello, World!',
+    }
+
+
+@bp.route('/hello-get')
+@token_required
+def hello_get():
+    return {
+        'error_code': 0,
+        'message': 'Hello, GET!',
+        'username': g.user.username,
+        'user_oid': g.user.oid,
+        'http_method': 'GET',
     }
--- tests/api/test_hello_json.py
+++ tests/api/test_hello_json.py
@@ -1,5 +1,31 @@
+import pytest
+
+
 def test_naked_get_says_hello(client):
     response = client.get('/api/hello')
     json = response.get_json()
     assert json['error_code'] == 0
     assert json['message'] == 'Hello, World!'
+
+
+@pytest.fixture
+def token(client):
+    response = client.post(
+        '/api/get_token',
+        json={
+            'username': 'test',
+            'password': 'test',
+        },
+    )
+    json = response.get_json()
+    return json['token']
+
+
+def test_token_get_returns_expected(token, client):
+    response = client.get('/api/hello-get', headers={'TOKEN': token})
+    assert response.status_code == 200
+    json = response.get_json()
+    assert json['error_code'] == 0
+    assert json['username'] == 'test'
+    assert json['user_oid'] > 0
+    assert json['http_method'] == 'GET'
