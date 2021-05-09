23685fc7fe776f8032a83b0de6ba93e4e8d7c58e
Eugene F. Barker <genebarker@gmail.com>
2021-05-09 20:33:27 +1300

    Remove unnecessary code

--- tests/client/test_http_request_provider.py
+++ tests/client/test_http_request_provider.py
@@ -3,22 +3,21 @@ import pytest
 from shrikenet.client.http_request_provider import HTTPRequestProvider
 
 
-class TestHTTPRequestProvider:
-
-    def test_interface_cant_be_instantiated(self):
-        with pytest.raises(NotImplementedError):
-            HTTPRequestProvider()
-
-    @pytest.fixture
-    def http(self):
-        class FakeAdapter(HTTPRequestProvider):
-            def __init__(self):
-                pass
-        return FakeAdapter()
+def test_interface_cant_be_instantiated():
+    with pytest.raises(NotImplementedError):
+        HTTPRequestProvider()
 
 
-    @pytest.mark.parametrize('http_method', ['get', 'post', 'put', 'delete'])
-    def test_get_cant_be_called(self, http, http_method):
-        http_call = getattr(http, http_method)  # i.e. http.get()
-        with pytest.raises(NotImplementedError):
-            http_call('http://example.com')
+@pytest.fixture
+def http():
+    class FakeAdapter(HTTPRequestProvider):
+        def __init__(self):
+            pass
+    return FakeAdapter()
+
+
+@pytest.mark.parametrize('http_method', ['get', 'post', 'put', 'delete'])
+def test_get_cant_be_called(http, http_method):
+    http_call = getattr(http, http_method)  # i.e. http.get()
+    with pytest.raises(NotImplementedError):
+        http_call('http://example.com')
