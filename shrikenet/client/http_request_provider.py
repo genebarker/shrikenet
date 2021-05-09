bace17fc6945d259c581b3d1623950e7c83ffc40
Eugene F. Barker <genebarker@gmail.com>
2021-05-09 21:04:32 +1300

    Add optional parms to http request provider

--- shrikenet/client/http_request_provider.py
+++ shrikenet/client/http_request_provider.py
@@ -3,14 +3,14 @@ class HTTPRequestProvider:
     def __init__(self):
         raise NotImplementedError
 
-    def get(self, url):
+    def get(self, url, json=None, token=None):
         raise NotImplementedError
 
-    def post(self, url):
+    def post(self, url, json=None, token=None):
         raise NotImplementedError
 
-    def put(self, url):
+    def put(self, url, json=None, token=None):
         raise NotImplementedError
 
-    def delete(self, url):
+    def delete(self, url, json=None, token=None):
         raise NotImplementedError
--- tests/client/test_http_request_provider.py
+++ tests/client/test_http_request_provider.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 
 from shrikenet.client.http_request_provider import HTTPRequestProvider
@@ -21,3 +22,13 @@ def test_methods_cant_be_called(http, http_method):
     http_call = getattr(http, http_method)  # i.e. http.get()
     with pytest.raises(NotImplementedError):
         http_call('http://example.com')
+
+
+@pytest.mark.parametrize('http_method', ['get', 'post', 'put', 'delete'])
+def test_methods_accept_parms(http, http_method):
+    http_call = getattr(http, http_method)  # i.e. http.get()
+    json_data = json.dumps({})
+    with pytest.raises(NotImplementedError):
+        http_call('http://example.com', json=json_data)
+    with pytest.raises(NotImplementedError):
+        http_call('http://example.com', json=json_data, token='tOkEn_data')
