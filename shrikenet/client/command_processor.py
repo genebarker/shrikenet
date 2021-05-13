98a13744ac0f7798c0cfa2d35411d62bc592931d
Eugene F. Barker <genebarker@gmail.com>
2021-05-14 09:52:17 +1300

    Change snet client into a class

---
+++ shrikenet/client/command_processor.py
@@ -0,0 +1,120 @@
+from configparser import ConfigParser
+from pathlib import Path
+import os
+import sys
+import textwrap
+
+import shrikenet
+
+
+DEFAULT_CONFIG_FILENAME = '.snetrc'
+
+
+class CommandProcessor:
+
+    def __init__(self, config_path_override=None):
+        self.config_path_override = config_path_override
+
+    def run(self, arg_list=None):
+        if arg_list and len(arg_list) > 1:
+            command = arg_list[1]
+            command_arg_list = arg_list[2:]
+            try:
+                func = getattr(self, f'{command}_cmd')
+                func(command_arg_list)
+            except AttributeError:
+                self.print_header()
+                self.eprint(
+                    f"ERROR: Unknown command ({command}) provided. Type 'snet "
+                    "help' for help."
+                )
+                sys.exit(1)
+
+        self.help_cmd(exit_code=1)
+
+    def print_header(self):
+        version = shrikenet.__version__
+        text = textwrap.dedent(f"""\
+            snet v{version} - The command line client for shrikenet.
+            Copyright (C) 2021 Eugene F. Barker. Web: https://github.com/genebarker
+            This program comes with ABSOLUTELY NO WARRANTY. This is free software.
+            Type 'snet license' for details.
+            """)
+        print(text)
+
+    def eprint(self, *args, **kwargs):
+        print(*args, file=sys.stderr, **kwargs)
+
+    def help_cmd(self, arg_list=None, exit_code=0):
+        self.print_header()
+        self.print_usage()
+        sys.exit(exit_code)
+
+    def print_usage(self):
+        text = textwrap.dedent("""\
+            Usage:
+              snet status
+              snet open [user@host[:port]]
+              snet close [user@host]
+              snet license
+              snet version
+              snet help [command]
+            """)
+        print(text)
+
+    def license_cmd(self, arg_list=None):
+        self.print_header()
+        license_filepath = self.get_license_filepath()
+        with open(license_filepath) as text_file:
+            print(text_file.read())
+        sys.exit(0)
+
+    def get_license_filepath(self):
+        this_path = os.path.dirname(__file__)
+        return os.path.join(this_path, '../../LICENSE.md')
+
+    def version_cmd(self, arg_list=None):
+        version = shrikenet.__version__
+        print(f'snet v{version}')
+        sys.exit(0)
+
+    def open_cmd(self, arg_list=None):
+        if arg_list is None or len(arg_list) == 0:
+            self.eprint(
+                'ERROR: A target account ID (i.e. me@example.com) must '
+                'be provided.'
+            )
+            sys.exit(1)
+
+        account_name = arg_list[0]
+        self.update_config_file_for_open(account_name)
+        self.print_open_successful(account_name)
+        sys.exit(0)
+
+    def update_config_file_for_open(self, account_name):
+        config = ConfigParser()
+        config[account_name] = {}
+        config[account_name]['is_open'] = 'true'
+        config[account_name]['token'] = 'fake_token'
+        config_path = self.get_config_path()
+        with open(config_path, 'w') as configfile:
+            config.write(configfile)
+
+    def get_config_path(self):
+        if self.config_path_override is not None:
+            return self.config_path_override
+        return Path(DEFAULT_CONFIG_FILENAME)
+
+    def print_open_successful(self, account_name):
+        username = self.get_username(account_name)
+        hostname = self.get_hostname(account_name)
+        print(f"Opened '{username}' at '{hostname}'")
+
+    def get_username(self, account_name):
+        chunk = account_name.split('@')
+        return chunk[0]
+
+    def get_hostname(self, account_name):
+        chunk_one = account_name.split('@')
+        chunk_two = chunk_one[1].split(':')
+        return chunk_two[0]
--- tests/client/test_option_parsing.py
+++ tests/client/test_option_parsing.py
@@ -1,10 +1,12 @@
-from configparser import ConfigParser
+import configparser
 import os
+import pathlib
+import tempfile
 
 import pytest
 
 import shrikenet
-from shrikenet.client import snet
+from shrikenet.client.command_processor import CommandProcessor
 
 
 TEST_USER = 'fmulder'
@@ -13,19 +15,27 @@ TEST_PORT = '5000'
 ACCOUNT_NAME = f'{TEST_USER}@{TEST_HOSTNAME}:{TEST_PORT}'
 
 
-def test_error_code_when_no_command():
-    exit_code = run_snet()
+@pytest.fixture
+def config():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        config_path = pathlib.Path(temp_dir) / 'testconfig'
+        yield config_path
+
+
+def test_error_code_when_no_command(config):
+    exit_code = run_snet(config)
     assert exit_code == 1
 
 
-def run_snet(args=None):
+def run_snet(config, args=None):
+    processor = CommandProcessor(config_path_override=config)
     with pytest.raises(SystemExit) as excinfo:
-        snet.main(args)
+        processor.run(args)
     return excinfo.value.code
 
 
-def test_usage_info_when_no_command(capsys):
-    run_snet()
+def test_usage_info_when_no_command(capsys, config):
+    run_snet(config)
     captured = capsys.readouterr()
     assert_has_header(captured.out)
     assert 'Usage:' in captured.out
@@ -35,18 +45,18 @@ def assert_has_header(output):
     assert 'snet' in output
 
 
-def test_error_code_on_unknown_command():
-    exit_code = run_snet_cmd('bogus')
+def test_error_code_on_unknown_command(config):
+    exit_code = run_snet_cmd('bogus', config)
     assert exit_code == 1
 
 
-def run_snet_cmd(cmd):
+def run_snet_cmd(cmd, config):
     args = ['snet', cmd] if isinstance(cmd, str) else ['snet'] + cmd
-    return run_snet(args)
+    return run_snet(config, args)
 
 
-def test_shows_header_with_error_on_unknown_command(capsys):
-    run_snet_cmd('bogus')
+def test_shows_header_with_error_on_unknown_command(config, capsys):
+    run_snet_cmd('bogus', config)
     captured = capsys.readouterr()
     assert_has_header(captured.out)
     assert (
@@ -60,41 +70,41 @@ def test_shows_header_with_error_on_unknown_command(capsys):
     ('version'),
     ('help'),
 ))
-def test_no_error_code(command):
-    error_code = run_snet_cmd(command)
+def test_no_error_code(command, config):
+    error_code = run_snet_cmd(command, config)
     assert error_code == 0
 
 
-def test_license_command_shows_it_with_header(capsys):
-    run_snet_cmd('license')
+def test_license_command_shows_it_with_header(config, capsys):
+    run_snet_cmd('license', config)
     captured = capsys.readouterr()
     assert_has_header(captured.out)
     assert 'GNU AFFERO' in captured.out
 
 
-def test_license_shows_regardless_of_run_path(capsys):
+def test_license_shows_regardless_of_run_path(config, capsys):
     homepath = os.environ['HOME']
     os.chdir(homepath)
-    run_snet_cmd('license')
+    run_snet_cmd('license', config)
     captured = capsys.readouterr()
     assert 'GNU AFFERO' in captured.out
 
 
-def test_version_command_shows_it(capsys):
-    run_snet_cmd('version')
+def test_version_command_shows_it(config, capsys):
+    run_snet_cmd('version', config)
     captured = capsys.readouterr()
     assert f'snet v{shrikenet.__version__}' in captured.out
 
 
-def test_help_shows_usage_info_with_header(capsys):
-    run_snet_cmd('help')
+def test_help_shows_usage_info_with_header(config, capsys):
+    run_snet_cmd('help', config)
     captured = capsys.readouterr()
     assert_has_header(captured.out)
     assert 'Usage:' in captured.out
 
 
-def test_open_no_args_shows_error(capsys):
-    error_code = run_snet_cmd('open')
+def test_open_no_args_shows_error(config, capsys):
+    error_code = run_snet_cmd('open', config)
     captured = capsys.readouterr()
     assert error_code == 1
     assert (
@@ -103,17 +113,17 @@ def test_open_no_args_shows_error(capsys):
     )
 
 
-def test_good_open_stores_account_info_in_config():
-    run_snet_cmd(['open', ACCOUNT_NAME])
-    config = ConfigParser()
-    config.read('.snetrc')
-    assert ACCOUNT_NAME in config.sections()
-    assert config[ACCOUNT_NAME].getboolean('is_open')
-    assert 'token' in config[ACCOUNT_NAME]
+def test_good_open_stores_account_info_in_config(config):
+    run_snet_cmd(['open', ACCOUNT_NAME], config)
+    parser = configparser.ConfigParser()
+    parser.read(config)
+    assert ACCOUNT_NAME in parser.sections()
+    assert parser[ACCOUNT_NAME].getboolean('is_open')
+    assert 'token' in parser[ACCOUNT_NAME]
 
 
-def test_good_open_returns_expected_output(capsys):
-    error_code = run_snet_cmd(['open', ACCOUNT_NAME])
+def test_good_open_returns_expected_output(config, capsys):
+    error_code = run_snet_cmd(['open', ACCOUNT_NAME], config)
     captured = capsys.readouterr()
     assert error_code == 0
     assert f"Opened '{TEST_USER}' at '{TEST_HOSTNAME}'" in captured.out
