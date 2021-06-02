2d5feafc58bc2ac9e556712b02ea6a7ecae5f0df
Eugene F. Barker <genebarker@gmail.com>
2021-06-02 17:01:51 +1300

    Update class to use improved class name

--- shrikenet/entities/event_validator.py
+++ shrikenet/entities/event_validator.py
@@ -4,25 +4,25 @@ from shrikenet.entities.record_validator import RecordValidator
 from shrikenet.entities.storage_provider import StorageProvider
 
 
-class EventValidator(RecordValidator):
+class LogEntryValidator(RecordValidator):
 
     text_min_length = 1
     text_max_length = 200
 
     @staticmethod
     def validate_fields(the_object: LogEntry):
-        event = the_object
-        FieldValidator.validate_oid(event.oid)
-        FieldValidator.validate_instant(event.time, 'time')
-        FieldValidator.validate_oid(event.app_user_oid)
-        FieldValidator.validate_tag(event.tag)
-        FieldValidator.validate_tag(event.usecase_tag, 'usecase_tag')
-        FieldValidator.validate_string(event.text, 'text',
-                                       EventValidator.text_min_length,
-                                       EventValidator.text_max_length)
+        log_entry = the_object
+        FieldValidator.validate_oid(log_entry.oid)
+        FieldValidator.validate_instant(log_entry.time, 'time')
+        FieldValidator.validate_oid(log_entry.app_user_oid)
+        FieldValidator.validate_tag(log_entry.tag)
+        FieldValidator.validate_tag(log_entry.usecase_tag, 'usecase_tag')
+        FieldValidator.validate_string(log_entry.text, 'text',
+                                       LogEntryValidator.text_min_length,
+                                       LogEntryValidator.text_max_length)
 
     @staticmethod
     def validate_references(the_object: LogEntry,
                             storage_provider: StorageProvider):
-        event = the_object
-        storage_provider.get_app_user_by_oid(event.app_user_oid)
+        log_entry = the_object
+        storage_provider.get_app_user_by_oid(log_entry.app_user_oid)
--- tests/entities/test_event_validator.py
+++ tests/entities/test_event_validator.py
@@ -2,7 +2,7 @@ import pytest
 
 from shrikenet.adapters.memory import Memory
 from shrikenet.entities.app_user import AppUser
-from shrikenet.entities.event_validator import EventValidator
+from shrikenet.entities.event_validator import LogEntryValidator
 from shrikenet.entities.record_validator import RecordValidator
 
 from tests.entities.test_log_entry import create_good_log_entry
@@ -10,81 +10,81 @@ from tests.entities.test_log_entry import create_good_log_entry
 
 # pylint: disable=redefined-outer-name
 @pytest.fixture
-def event():
+def log_entry():
     return create_good_log_entry()
 
 
 def test_is_a_record_validator():
-    assert issubclass(EventValidator, RecordValidator)
+    assert issubclass(LogEntryValidator, RecordValidator)
 
 
-def test_successful_validation_returns_none(event):
-    assert EventValidator.validate_fields(event) is None
+def test_successful_validation_returns_none(log_entry):
+    assert LogEntryValidator.validate_fields(log_entry) is None
 
 
-def test_oid_required(event):
-    event.oid = None
-    verify_validation_raises(event)
+def test_oid_required(log_entry):
+    log_entry.oid = None
+    verify_validation_raises(log_entry)
 
 
-def verify_validation_raises(event):
+def verify_validation_raises(log_entry):
     with pytest.raises(ValueError):
-        EventValidator.validate_fields(event)
+        LogEntryValidator.validate_fields(log_entry)
 
 
-def test_oid_validated(event):
-    event.oid = 'bad'
-    verify_validation_raises(event)
+def test_oid_validated(log_entry):
+    log_entry.oid = 'bad'
+    verify_validation_raises(log_entry)
 
 
-def test_time_required(event):
-    event.time = None
-    verify_validation_raises(event)
+def test_time_required(log_entry):
+    log_entry.time = None
+    verify_validation_raises(log_entry)
 
 
-def test_time_validated(event):
-    event.time = 123
-    verify_validation_raises(event)
+def test_time_validated(log_entry):
+    log_entry.time = 123
+    verify_validation_raises(log_entry)
 
 
-def test_app_user_id_required(event):
-    event.app_user_oid = None
-    verify_validation_raises(event)
+def test_app_user_id_required(log_entry):
+    log_entry.app_user_oid = None
+    verify_validation_raises(log_entry)
 
 
-def test_app_user_id_validated(event):
-    event.app_user_oid = 'bad'
-    verify_validation_raises(event)
+def test_app_user_id_validated(log_entry):
+    log_entry.app_user_oid = 'bad'
+    verify_validation_raises(log_entry)
 
 
-def test_tag_required(event):
-    event.tag = None
-    verify_validation_raises(event)
+def test_tag_required(log_entry):
+    log_entry.tag = None
+    verify_validation_raises(log_entry)
 
 
-def test_tag_validated(event):
-    event.tag = 'Bad tag'
-    verify_validation_raises(event)
+def test_tag_validated(log_entry):
+    log_entry.tag = 'Bad tag'
+    verify_validation_raises(log_entry)
 
 
-def test_usecase_tag_required(event):
-    event.usecase_tag = None
-    verify_validation_raises(event)
+def test_usecase_tag_required(log_entry):
+    log_entry.usecase_tag = None
+    verify_validation_raises(log_entry)
 
 
-def test_usecase_tag_validated(event):
-    event.usecase_tag = 'Bad tag'
-    verify_validation_raises(event)
+def test_usecase_tag_validated(log_entry):
+    log_entry.usecase_tag = 'Bad tag'
+    verify_validation_raises(log_entry)
 
 
-def test_text_required(event):
-    event.text = None
-    verify_validation_raises(event)
+def test_text_required(log_entry):
+    log_entry.text = None
+    verify_validation_raises(log_entry)
 
 
-def test_text_validated(event):
-    event.text = 'a' * (EventValidator.text_max_length + 1)
-    verify_validation_raises(event)
+def test_text_validated(log_entry):
+    log_entry.text = 'a' * (LogEntryValidator.text_max_length + 1)
+    verify_validation_raises(log_entry)
 
 
 @pytest.fixture
@@ -95,18 +95,18 @@ def db():
     provider.close()
 
 
-def test_unknown_app_user_oid_raises(event, db):
+def test_unknown_app_user_oid_raises(log_entry, db):
     with pytest.raises(Exception) as excinfo:
-        EventValidator.validate_references(event, db)
+        LogEntryValidator.validate_references(log_entry, db)
 
     expected_message = (
         'can not get app_user (oid={}), reason: '
-        .format(event.app_user_oid)
+        .format(log_entry.app_user_oid)
     )
     assert expected_message in str(excinfo.value)
 
 
-def test_known_app_user_validates(event, db):
-    user = AppUser(event.app_user_oid, None, event.app_user_name, None)
+def test_known_app_user_validates(log_entry, db):
+    user = AppUser(log_entry.app_user_oid, None, log_entry.app_user_name, None)
     db.add_app_user(user)
-    assert EventValidator.validate_references(event, db) is None
+    assert LogEntryValidator.validate_references(log_entry, db) is None
