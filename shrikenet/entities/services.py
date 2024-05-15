2d8a8a13ed77d24ff1b2f17773ff6a3f90a9e8ad
Eugene F. Barker <genebarker@gmail.com>
2024-05-15 15:22:20 +1300

    Add password checker to shrikenet services

--- shrikenet/entities/services.py
+++ shrikenet/entities/services.py
@@ -5,7 +5,9 @@ class Services:
         storage_provider=None,
         text_transformer=None,
         crypto_provider=None,
+        password_checker=None,
     ):
         self.storage_provider = storage_provider
         self.text_transformer = text_transformer
         self.crypto_provider = crypto_provider
+        self.password_checker = password_checker
